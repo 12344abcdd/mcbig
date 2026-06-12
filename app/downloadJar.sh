44a1da10e6dccc2837c6550401839f410bb60a6b
Your Name <you@example.com>
2026-06-12 17:31:35 +0800

    -

---
+++ app/downloadJar.sh
@@ -0,0 +1,7 @@
+mkdir -p mc_jars && cd mc_jars
+
+# 1. 下载主客户端jar
+jq -r '.downloads.client.url' ../version.json | xargs -I {} wget -c {}
+
+# 2. 下载所有依赖库jar
+jq -r '.libraries[].downloads.artifact.url | select(.)' ../version.json | xargs -L1 wget -c
